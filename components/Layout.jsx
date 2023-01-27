9a5373167f8867ab67a28875a571ddf3be816825
astabidasaaa <astabidasabcd@gmail.com>
2023-01-28 00:32:17 +0700

    add new font

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { poppins } from "./FontSrc";
+import { poppins, montserrat } from "./FontSrc";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
@@ -7,11 +7,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main className={`${poppins.variable} font-poppins`}>{children}</main>
+      <main className={`${montserrat.variable} font-montserrat`}>
+        {children}
+      </main>
       <Footer />
     </>
   );
 };
 
 export default Layout;
-// className = "pt-20 sm:pt-28 lg:pt-32 px-4 sm:px-8 lg:px-20";
