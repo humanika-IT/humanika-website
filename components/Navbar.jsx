e878824af3eea2dde9b4e7d4b63b4d73803ba3cb
astabidasaaa <astabidasabcd@gmail.com>
2023-01-28 00:33:04 +0700

    chg navbar layout for mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,71 +9,77 @@ import { poppins } from "./FontSrc";
 const Navbar = () => {
   // ============ HIDE MENU AT LOGO CLICK =============
   const [isOpen, setNav] = useState(false);
+
   const toggleNav = () => {
     setNav(!isOpen);
   };
   // ==================================================
 
   // =============== NAVBAR HIDDEN ===============
-  const [show, setShow] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
+  // const [show, setShow] = useState(true);
+  // const [lastScrollY, setLastScrollY] = useState(0);
 
-  useEffect(() => {
-    const controlNavbar = () => {
-      if (typeof window !== "undefined") {
-        if (window.scrollY > 40) {
-          setShow(false);
-        } else {
-          setShow(true);
-        }
+  // useEffect(() => {
+  //   const controlNavbar = () => {
+  //     if (typeof window !== "undefined") {
+  //       if (window.scrollY > 40) {
+  //         setShow(false);
+  //       } else {
+  //         setShow(true);
+  //       }
 
-        setLastScrollY(window.scrollY);
-      }
-    };
+  //       setLastScrollY(window.scrollY);
+  //     }
+  //   };
 
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", controlNavbar);
+  //   if (typeof window !== "undefined") {
+  //     window.addEventListener("scroll", controlNavbar);
 
-      // cleanup function
-      return () => {
-        window.removeEventListener("scroll", controlNavbar);
-      };
-    }
-  }, [lastScrollY]);
+  //     // cleanup function
+  //     return () => {
+  //       window.removeEventListener("scroll", controlNavbar);
+  //     };
+  //   }
+  // }, [lastScrollY]);
   // ==================================================
 
   return (
     // <header className={show ? layout.header : layout.header_scroll}>
-    <header className={`${poppins.variable} font-poppins ${layout.header}`}>
-      <Link
-        href="/"
-        prefetch={false}
-        passHref
-        onClick={() => setNav(false)}
-        className={layout.logo_container}
-      >
-        <Image
-          src="/logo/logo_humanika_new.png"
-          width={510}
-          height={160}
-          alt="Logo Humanika"
-          className={layout.logo_image}
-        />
-      </Link>
-      <nav className="block lg:hidden">
+    <>
+      <header className={`${poppins.variable} font-poppins ${layout.header}`}>
+        <Link
+          href="/"
+          prefetch={false}
+          passHref
+          onClick={() => setNav(false)}
+          className={layout.logo_container}
+        >
+          <Image
+            src="/logo/logo_humanika_new.png"
+            width={510}
+            height={160}
+            alt="Logo Humanika"
+            className={layout.logo_image}
+          />
+        </Link>
+        <nav className="block lg:hidden">
+          <MenuBtn toggleNav={toggleNav} isOpen={isOpen} />
+        </nav>
+        <nav className="hidden lg:block">
+          <MenuBar setNav={setNav} isOpen={true} />
+        </nav>
+      </header>
+      <nav className={`${poppins.variable} font-poppins block lg:hidden`}>
         <div
           className={
             isOpen ? layout.menubar_container : layout.menubar_container_close
           }
         >
-          <MenuBar toggleNav={toggleNav} isOpen={isOpen} />
+          <MenuBtn toggleNav={toggleNav} isOpen={isOpen} />
+          <MenuBar setNav={setNav} isOpen={isOpen} />
         </div>
-        <MenuBtn toggleNav={toggleNav} isOpen={isOpen} />
       </nav>
-      <nav className="hidden lg:block">
-        <MenuBar toggleNav={toggleNav} isOpen={true} />
-      </nav>
-    </header>
+    </>
   );
 };
 
